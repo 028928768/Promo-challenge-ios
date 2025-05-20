21d32d7b59fa12f6b3dd425c5e736bda3319486d
Simon_996 <kantachat_2539@hotmail.com>
2025-05-20 00:19:50 -0700

    Initial Commit

---
+++ primo-challenge-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  primo-challenge-ios
+//
+//  Created by Simon SIwell on 20/5/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ primo-challenge-ios/primo_challenge_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  primo_challenge_iosApp.swift
+//  primo-challenge-ios
+//
+//  Created by Simon SIwell on 20/5/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct primo_challenge_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
