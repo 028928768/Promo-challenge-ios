163443320d4e885ae6c3db3b6f868b1a35b30d01
Simon_996 <kantachat_2539@hotmail.com>
2025-05-20 04:02:20 -0700

    [Primo-001] - fetch data remotely and display on articleListView, articleCardView, articleDetailView

---
+++ primo-challenge-ios/Presentation/ArticleList/View/ArticleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailView.swift
+//  primo-challenge-ios
+//
+//  Created by Simon SIwell on 20/5/2568 BE.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDetailView()
+}
